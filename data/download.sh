59eb60d8b82876022c6c73973b2724765527f9fb
Ruopu Chu <kylianchucs@outlook.com>
2024-05-29 15:46:38 +0800

    fix bug

--- data/download.sh
+++ data/download.sh
@@ -1,4 +1,6 @@
-kaggle datasets download -d alessiocorrado99/animals10
-unzip -d ./animals10/ ./animals10.zip
-rm animals10.zip
+if [ ! -d "./animals10/" ]; then
+  kaggle datasets download -d alessiocorrado99/animals10
+  unzip -d ./animals10/ ./animals10.zip
+  rm animals10.zip
+fi
 python split.py
